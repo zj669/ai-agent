1ba2ba7d9e87388eaa739b441a59898755bdd4e3
zj669 <3218356902@qq.com>
2026-01-04 15:10:00 +0800

    refaactor(-): 移除旧代码

--- aiagemt/src/main/java/com/zj/aiagemt/common/design/singletonresponsibilitychain/ILogicLink.java
+++
@@ -1,9 +0,0 @@
-package com.zj.aiagemt.common.design.singletonresponsibilitychain;
-
-
-
-public interface ILogicLink<REQUEST, CONTEXT, RESULT> extends ILogicChainArmory<REQUEST, CONTEXT, RESULT> {
-
-    RESULT apply(REQUEST request, CONTEXT dynamicContext);
-
-}
--- aiagemt/src/main/java/com/zj/aiagemt/service/agent/impl/armory/node/PromptNode.java
+++
@@ -1,4 +0,0 @@
-package com.zj.aiagemt.service.agent.impl.armory.node;
-
-public class PromptNode {
-}
